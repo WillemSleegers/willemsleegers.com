34bf3fe59803a53fd9394317b75c0bcafab7b18b
willemsleegers <w.sleegers@me.com>
2021-06-22 22:54:21 +0200

    Fixed some code/highlight issues.

--- themes/me/static/js/prism.js
+++ themes/me/static/js/prism.js
@@ -430,7 +430,7 @@ Prism.languages.r = {
   ],
   keyword:
     /\b(?:if|else|repeat|while|for|in|next|break|NULL|NA|NA_integer_|NA_real_|NA_complex_|NA_character_)\b/,
-  function: /\.?\w+(?=\()/,
+  function: /\.?\w+(?=\()|t.test/,
   operator: /->?>?|<(?:=|<?-)?|[>=!]=?|::?|&&?|\|\|?|[+*\/^$@~]/,
   punctuation: /[(){}\[\],;]/,
 }
