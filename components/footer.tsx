a60900c26781e0468b82c2b3d62eace3ffd314e6
willemsleegers <w.sleegers@me.com>
2025-01-29 10:08:04 +0100

    Layout tweaks

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="text-center pt-12 pb-16 text-muted-foreground">
+    <footer className="text-center mb-16 text-muted-foreground">
       Willem Sleegers &copy; 2024
     </footer>
   )
